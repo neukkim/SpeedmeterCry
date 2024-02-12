a576ca1110780c634cf8e25f3e973d44a1ab9749
sunminhong <akoda5555@gmail.com>
2024-02-13 02:55:49 +0900

    mi/h Add

--- SpeedmeterCry/ContentView.swift
+++ SpeedmeterCry/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+  @ObservedObject var locationManager = LocationManager()
+  var speed: String { return locationManager.speed }
+  var speedColor: Color { return locationManager.speedColor }
+  var speedAccuracy: String { return locationManager.speedAccuracy }
+  let plusMinus = "\u{00B1}"
+  
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+      VStack {
+          VStack {
+              Text(speed)
+                  .foregroundColor(speedColor)
+                  .font(.system(size: 500, design: .rounded))
+                  .minimumScaleFactor(0.01)
+                  .lineLimit(1)
+              Text("mi/h")
+                  .font(.system(size: 50, design: .rounded))
+                  .padding(.bottom)
+              Text("Error: \(plusMinus)\(speedAccuracy) mi/h")
+                  .font(.system(size: 20, design: .rounded))
+                  .padding(.bottom)
+          }
+      }
+  }
 }
-
 #Preview {
     ContentView()
 }
+
