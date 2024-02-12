30f7f28a57d120c8b96f200cd71c03e91942d5f3
sunminhong <akoda5555@gmail.com>
2024-02-12 21:02:19 +0900

    Initial Commit

---
+++ SpeedmeterCry/SpeedmeterCryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedmeterCryApp.swift
+//  SpeedmeterCry
+//
+//  Created by SunMin Hong on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedmeterCryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
